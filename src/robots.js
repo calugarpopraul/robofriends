20096d71381da1f9ea69db4d0a5a2e8490c6452c
user <calugarpopraul@gmail.com>
2019-01-26 23:02:25 +0200

    Cards

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Marry Gold',
+        username: 'Gold',
+        email: 'gold@april.biz'
+    },
+    {
+        id: 3,
+        name: 'John Doe',
+        username: 'John',
+        email: 'john@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Rob Bold',
+        username: 'Bold',
+        email: 'Bold@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Anna Julia',
+        username: 'Julia',
+        email: 'julia@april.biz'
+    }
+];
